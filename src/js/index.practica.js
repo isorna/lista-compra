c5523286d10e90e11e0af7cebb6029c695c70f12
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-01-14 13:32:51 +0100

    feat(practicas): lista de la compra

--- src/js/index.practica.js
+++ src/js/index.practica.js
@@ -53,18 +53,14 @@ let shoppingListWithObjects = [
 
 console.log('LISTA DE LA COMPRA POR DEFECTO', shoppingList)
 
-// TOMORROW:
 // Add current article to shopping list
 function addToShoppingList() {
   // Add to shopping list as text string
   let articleName = document.getElementById('article').value
   let articleQty = document.getElementById('qty').value
   let articlePrice = document.getElementById('price').value
-  let logText = document.getElementById('log')
-  let shoppingListTable = document.getElementById('shoppingListTable')
   let shoppingListTableBody = document.getElementById('shoppingListTableBody')
   let shoppingListTableTotal = document.getElementById('shoppingListTableTotal')
-  let mainContent = document.getElementById('mainContent')
   let totalAmount = 0
   // Define new article object
   let newArticleObject = {
@@ -116,46 +112,25 @@ function addToShoppingList() {
   shoppingListTableTotal.innerText = totalAmount
 
   // Create new article on table
-  let newArticleElement = document.createElement('p')
-  newArticleElement.innerText = 'He añadido '
-  + articleQty
-  + ' unidades de '
-  + articleName
-  + ' al precio de '
-  + articlePrice
-  + ' €'
-  mainContent.appendChild(newArticleElement)
-  // 1. Creo la fila
+  // 1. Create table row
   let newTableRow = document.createElement('tr')
-  // 2. Creo las celdas
+  // 2. Create row cells
   let qtyCell = document.createElement('td')
   let nameCell = document.createElement('td')
   let priceCell = document.createElement('td')
   let subtotalCell = document.createElement('td')
-  // 3. Añado los valores a las celdas
+  // 3. Add cell values
   qtyCell.innerText = newArticleObject.qty
   nameCell.innerText = newArticleObject.name
   priceCell.innerText = newArticleObject.price
-  subtotalCell.innerText = newArticleObject.qty * newArticleObject.price
-  // 4. Añado las celdas a la fila
+  subtotalCell.innerText = (newArticleObject.qty * newArticleObject.price) + ' €'
+  // 4. Add cells to table row
   newTableRow.appendChild(qtyCell)
   newTableRow.appendChild(nameCell)
   newTableRow.appendChild(priceCell)
   newTableRow.appendChild(subtotalCell)
-  // 5. Añado la fila a shoppingListTableBody
+  // 5. Add table row to shoppingListTableBody
   shoppingListTableBody.appendChild(newTableRow)
-
-  // Log
-  logText.innerText = 'He añadido '
-  + articleQty
-  + ' unidades de '
-  + articleName
-  + ' al precio de '
-  + articlePrice
-  + ' €'
-  // console.log('TABLA', shoppingListTable)
-  // console.log('addToShoppingList NEW ARTICLE', newArticleObject)
-  // console.log('addToShoppingList SHOPPING LIST', shoppingList)
 }
 
 function resetShoppingList() {
