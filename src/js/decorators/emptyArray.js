a1aacbd253569be77f313ef61f0e2b87ec1ec591
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-01-23 16:23:19 +0100

    feat(practicas): :sparkles: JSDOC

--- src/js/decorators/emptyArray.js
+++ src/js/decorators/emptyArray.js
@@ -1,13 +1,14 @@
+// @ts-nocheck
+
 /**
  * Decorator that adds an empty method to an array. This method removes all
  * elements from the array.
- * @param {Array} array The array to decorate.
- * @returns {Array} The decorated array.
  */
 export function emptyArray(array) {
   /**
    * Removes all elements from the array.
    */
+
   array.empty = function() {
     while (this.length > 0) {
       this.pop()
