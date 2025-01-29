ae43adddd46778e0175e9c434ecd47fdc675027b
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-01-29 16:21:25 +0100

    feat(practicas): :sparkles: configuracion tailwindcss

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,14 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: ["stylelint-config-standard"],
+  // extends: ["stylelint-config-tailwindcss"],
   ignoreFiles: [
       "src/css/output.css"
   ],
   rules: {
-    "alpha-value-notation": "number"
+    "alpha-value-notation": "number",
+    "at-rule-no-unknown": [true, {
+      "ignoreAtRules": ["tailwind"]
+    }]
   }
 };
