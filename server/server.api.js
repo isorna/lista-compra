68140635b990c15e7e93d289df3177306bbbc7f0
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-02-05 12:20:59 +0100

    feat(practicas): :sparkles: servidor API REST de node - PUT & DELETE

--- server/server.api.js
+++ server/server.api.js
@@ -19,14 +19,30 @@ const MIME_TYPES = {
 // const USERS_URL = './server/BBDD/users.json'
 const ARTICLES_URL = './server/BBDD/articles.json'
 
+/**
+ * Returns an object with the action name and id from the given pathname.
+ * For example, for "/create/articles/:id", it will return { name: "/create/articles", id: ":id" }
+ * @param {string} pathname
+ * @returns {{name: string, id: string}}
+ */
+function getAction(pathname) {
+  // /create/articles/:id
+  const actionParts = pathname.split('/');
+  return {
+    name: `/${actionParts[1]}/${actionParts[2]}`,
+    id: actionParts[3]
+   }
+}
+
 http
   .createServer(async (request, response) => {
     const url = new URL(`http://${request.headers.host}${request.url}`);
     const urlParams = Object.fromEntries(url.searchParams);
+    const action = getAction(url.pathname);
     const statusCode = 200
     let responseData = []
     let chunks = []
-    console.log(request.method, url.pathname, urlParams);
+    console.log(request.method, url.pathname, urlParams, action.name, action.id);
     // Set Up CORS
     response.setHeader('Access-Control-Allow-Origin', '*');
     response.setHeader('Content-Type', MIME_TYPES.json);
@@ -43,13 +59,14 @@ http
     }
     // Continue on GET request
     // TODO: use POST/PUT/DELETE methods when needed
-    switch (url.pathname) {
+    switch (action.name) {
       case '/create/articles':
         request.on('data', (chunk) => {
           chunks.push(chunk)
         })
         request.on('end', () => {
           let body = Buffer.concat(chunks)
+          console.log('create article - body BUFFER', body)
           let parsedData = qs.parse(body.toString())
           console.log('create article - body', parsedData)
           crud.create(ARTICLES_URL, parsedData, (data) => {
@@ -60,6 +77,30 @@ http
             response.end();
           });
         })
+        break;
+      case '/update/articles':
+        console.log('UPDATE', action.id)
+        request.on('data', (chunk) => {
+          chunks.push(chunk)
+        })
+        request.on('end', () => {
+          let body = Buffer.concat(chunks)
+          console.log('update article - body BUFFER', body)
+          let parsedData = qs.parse(body.toString())
+          console.log('update article - body', parsedData)
+          // crud.create(ARTICLES_URL, parsedData, (data) => {
+          //   console.log(`server create article ${data.name} creado`, data)
+          //   responseData = data
+
+          //   response.write(JSON.stringify(responseData));
+          //   response.end();
+          // });
+          response.end();
+        })
+        break;
+      case '/delete/articles':
+        console.log('DELETE', action.id)
+        response.end();
         break;
       case '/read/articles':
         crud.read(ARTICLES_URL, (data) => {
