6a564eb2dc0a6c505ae509baf9c527b9fae10a71
Alvaro Isorna <alvaro.isorna@gmail.com>
2025-02-19 17:44:30 +0100

    build: :heavy_plus_sign: eslint for css

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import css from "@eslint/css";
+
+// INFO: https://eslint.org/blog/2025/02/eslint-css-support/?s=03
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  // lint js files
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  // lint css files
+  {
+    files: ["**/*.css"],
+    plugins: {
+      css,
+    },
+    language: "css/css",
+    rules: {
+      "css/no-duplicate-imports": "error",
+    },
+  },
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
